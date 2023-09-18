5ff30d06ee86f27e5879e9f8dc06f8ab3c08287d
Thom Oman <toman@gocardless.com>
2023-09-18 20:28:10 +0100

    pong

--- app/server.go
+++ app/server.go
@@ -6,18 +6,34 @@ import (
 	"os"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+const (
+	CONN_PORT = "6379"
+	CONN_TYPE = "tcp"
+)
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+func main() {
+	l, err := net.Listen(CONN_TYPE, "0.0.0.0:"+CONN_PORT)
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Println("Failed to bind to port " + CONN_PORT)
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleRequest(conn)
 	}
+}
+
+func handleRequest(conn net.Conn) {
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+	conn.Write([]byte("+PONG\r\n"))
+	conn.Close()
 }
