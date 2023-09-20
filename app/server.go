65459f33f0015bf7385348fd2de8606a5ca9057c
Thom Oman <toman@gocardless.com>
2023-09-20 13:30:06 +0100

    think this works for echo

--- app/server.go
+++ app/server.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"net"
 	"os"
+	"strconv"
 )
 
 const (
@@ -51,13 +53,59 @@ func handleConnection(conn net.Conn) {
 			os.Exit(1)
 		}
 		// fmt.Println(buf)
-		cmds := splitRequest(buf)
-		fmt.Println(cmds)
-		for i, _ := range cmds {
-			fmt.Println(string(cmds[i]))
+		req, _ := buildRequest(buf)
+		switch req.Command() {
+		case "PING":
+			writeResponse(conn, []byte("+PONG\r\n"))
+		case "ECHO":
+			writeResponse(conn, []byte(req.Args()[0]+"\r\n"))
 		}
-		writeResponse(conn, []byte("+PONG\r\n"))
+		fmt.Println(req)
+		// for i, _ := range req {
+		// 	fmt.Println(string(req[i]))
+		// }
 	}
+}
+
+type request interface {
+	Command() string
+	Args() []string
+	Simple() bool
+	// Add([]byte)
+}
+
+type simpleStringsRequest struct {
+	cmd string
+}
+
+func (r simpleStringsRequest) Command() string {
+	return r.cmd
+}
+
+func (r simpleStringsRequest) Args() []string {
+	return []string{}
+}
+
+func (r simpleStringsRequest) Simple() bool {
+	return true
+}
+
+type arrayRequest struct {
+	cmd  string
+	args []string
+	size int
+}
+
+func (r arrayRequest) Command() string {
+	return r.cmd
+}
+
+func (r arrayRequest) Args() []string {
+	return r.args
+}
+
+func (r arrayRequest) Simple() bool {
+	return false
 }
 
 func writeResponse(conn net.Conn, resp []byte) {
@@ -68,48 +116,72 @@ func writeResponse(conn net.Conn, resp []byte) {
 	}
 }
 
-func splitRequest(req []byte) [][]byte {
-	var cmds [][]byte
-	var cur []byte
-	bulk := isBulkRequest(req[0])
+func buildRequest(req []byte) (request, error) {
+	fmt.Println(req)
+	d := split(req[1:])
+	fmt.Println(d)
 
-	for i := range req {
-		if i == 0 {
-			continue
+	var r request
+
+	switch req[0] {
+	case 43: // +
+		// Simple strings	Simple	+
+		if len(d) != 1 {
+			fmt.Printf("Not sure we can have multiple arguments to a simple strings request")
+			os.Exit(1)
+		}
+		r = &simpleStringsRequest{cmd: string(d[0])}
+	case 42: // *
+		// Arrays	Aggregate	*
+		l, err := strconv.Atoi(string(d[0]))
+		if err != nil {
+			fmt.Printf("Could not convert %v to int: %v", d[0], err.Error())
+			os.Exit(1)
 		}
 
+		if l != len(d)-1 {
+			fmt.Printf("Expected %v elements but %v provided", l, len(d)-1)
+			os.Exit(1)
+		}
+		var args []string
+
+		for i := 2; i < len(d); i++ {
+			args = append(args, string(d[i]))
+		}
+
+		r = &arrayRequest{
+			cmd:  string(d[1]),
+			args: args,
+			size: l,
+		}
+	}
+	if r == nil {
+		return nil, errors.New(fmt.Sprintf("Invalid first token %v", req[0]))
+	}
+	return r, nil
+}
+
+func split(req []byte) [][]byte {
+	var cmds [][]byte
+	var cur []byte
+
+	for i := range req {
 		if req[i] == 0 {
 			break
 		}
-		if !bulk && (req[i] == 10 || req[i] == 13) {
-			fmt.Println("Cannot provide \\n or \\r")
-			os.Exit(1)
-		}
+		fmt.Printf("Appending %v to %v. Complete: %v\n", req[i], cur, cmds)
 		cur = append(cur, req[i])
 
 		if len(cur) >= 2 && bytes.Equal(cur[len(cur)-2:], SEPARATOR) {
-			if !bulk {
-				fmt.Printf("Complex command provided: %v\n", req[0])
-				os.Exit(1)
+			if len(cur[:len(cur)-2]) > 0 {
+				cmds = append(cmds, cur[:len(cur)-2])
 			}
-
-			cmds = append(cmds, cur[:len(cur)-2])
 			cur = nil
 		}
 	}
-
-	cmds = append(cmds, cur)
-	return cmds
-}
-
-func isBulkRequest(b byte) bool {
-	switch b {
-	case 43, 45, 58, 95, 35, 44, 40:
-		// simple
-		return false
-	case 36, 42, 33, 61, 37, 126, 62:
-		return true
-	default:
-		return false
+	if len(cur) != 0 {
+		// Should this ever be the case?
+		cmds = append(cmds, cur)
 	}
+	return cmds
 }
