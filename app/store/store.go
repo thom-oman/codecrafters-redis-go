df5a4a78b843cea3870a8dadc57e28cbee8e65d2
Thom Oman <toman@gocardless.com>
2023-09-22 12:38:16 +0100

    log all the pointers

--- app/store/store.go
+++ app/store/store.go
@@ -23,6 +23,9 @@ func (v *value) SetExpiry(t time.Time) {
 
 func Set(k, v string, px int) error {
 	val := &value{Data: v}
+	fmt.Printf("Val = %v", val)
+	fmt.Printf("&Val = %v", &val)
+	fmt.Printf("*Val = %v", *val)
 	if px > 0 {
 		val.SetExpiry(time.Now().Add(time.Millisecond * time.Duration(px)))
 	}
@@ -32,6 +35,9 @@ func Set(k, v string, px int) error {
 
 func Get(k string) (string, error) {
 	val := *store[k]
+	fmt.Printf("Val = %v", val)
+	fmt.Printf("&Val = %v", &val)
+	fmt.Printf("*Val = %v", *val)
 	fmt.Println("CHECKING IS ZERO")
 	if val.exp.IsZero() {
 		return val.Data, nil
